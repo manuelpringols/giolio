dca2b6aac00a466462f40f50ba4708a80478fe17
manuelpringols <manuelpringols@gmail.com>
2026-01-08 21:09:27 +0100

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1291, hash: '78d33e98cfe46020e36b8d7f6da473d2a264f0fb345ee409cd69adac78559311', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1033, hash: '2cb903731ac0a41de172291d0e08ec08a75a6b6d5218cf7ca77e6d9f7169f800', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 25993, hash: '00fb528db58c2f5f717cd01fff00d8c4b5f1da02ef8da214c8c712ba7b08538d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1291, hash: '755f1aecd290ad247c5f9414cb0f76de59c3a5b7ea2be75e58e65eb5c0e6fc5c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1033, hash: 'efbd0c84827f0c15fddffc8a9a87fedf1cfedee1fd650383f10be2b2cb61802e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 25978, hash: '8b9d952035e732a4471670bf90b1c23e075ece78a90b87a78ba4ab60fa2de4e9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-QBFKMMZL.css': {size: 2010, hash: 'udV8tLGc2HY', text: () => import('./assets-chunks/styles-QBFKMMZL_css.mjs').then(m => m.default)}
   },
 };
