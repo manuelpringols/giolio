64579469894327e5b0f9fbe49dc728b23b95a665
manuelpringols <manuelpringols@gmail.com>
2026-01-09 16:05:17 +0100

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1441, hash: '292f6c9c81847315a393a602037d68892510f975284a3040d5e18412a28ec2d8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1041, hash: '779a219e7d7311dbdb3bccdae92c45de84d777ebb64aa90f9c689b5ebc4155a5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 27511, hash: 'e7ec4335c7c45f20bacde59a8d33bec7cbadb1ffbb430ae3768b8c83c15c76d9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1441, hash: 'b92e1786907d837e66a6c6cc557735afcb59155adca1990b620b35c28abda800', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1041, hash: '195d86ba1ac26d666481f30a453eb9a9ece1c370b22126000e319ac7ebc66b6d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 27511, hash: '849f18db52dd5a6fd39ef7126e2dc33bbf0a22b391899c1e4b1e549fb00cb95f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-F5I6R27F.css': {size: 2250, hash: 'SG/5mb3oUmk', text: () => import('./assets-chunks/styles-F5I6R27F_css.mjs').then(m => m.default)}
   },
 };
--- server/assets-chunks/index_csr_html.mjs
+++ server/assets-chunks/index_csr_html.mjs
@@ -11,6 +11,6 @@ export default `<!doctype html>
 <style>html,body{margin:0;padding:0;height:100%;overflow-x:hidden}body{position:relative;background:linear-gradient(135deg,#f0ccdc,#5a65a3d9);background-repeat:no-repeat;background-attachment:fixed;background-size:cover;z-index:1000}body:before{content:"";position:fixed;top:0;left:0;width:200%;height:100%;background-image:url(https://www.transparenttextures.com/patterns/gray-floral.png);background-repeat:repeat-x;background-size:contain;filter:blur(5.5px) saturate(.9)!important;opacity:.95!important;opacity:.8;z-index:-1;pointer-events:none;animation:move-bg 30s linear infinite}body:hover:before{filter:blur(0) saturate(1)!important;opacity:1!important}@keyframes move-bg{0%{transform:translate(0)}to{transform:translate(-50%)}}@media(max-width:640px){body{background-attachment:scroll}}</style><link rel="stylesheet" href="styles-F5I6R27F.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-F5I6R27F.css"></noscript></head>
 <body ngcm="">
   <app-root></app-root>
-<script src="main-JC445HNY.js" type="module"></script></body>
+<script src="main-5HWC67WI.js" type="module"></script></body>
 </html>
 `;
